b37a302f7a05c3c2a64c28fc05666fe9ce1b583f
stephenAmp <stephen013amponsem@gmail.com>
2024-10-28 11:53:59 +0000

    Added css styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import 'bootstrap' 
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import { useState } from 'react';
 
 export default function App(){
     const [ activity,setActivity ] = useState('')
     const [ submitActivity,setSubmitActivity ] = useState([])
 
+    let inputBoxEmpty;
+    if(activity === ''){
+        inputBoxEmpty = false
+    }else{
+        inputBoxEmpty = true
+    }
+    
     function handleChange(e){
         setActivity(e.target.value)
 
@@ -38,19 +47,26 @@ export default function App(){
         }
         
     }
-    
+
     return(
-        <>
-            <label>Activity:</label>
-            <input type='text' value={activity} onKeyDown={handleKey} onChange={handleChange}/>
+<>
+    <div className="container" style={{marginTop:10}}>   
+        <div className="mb-3">    
+            <label for='activity' className="form-label"><b>Activity</b></label>
+            <input id ='activity' className = 'form-control' type='text' value={activity} onKeyDown={handleKey} onChange={handleChange}/>
+        </div>
+        {inputBoxEmpty ? (
             <button onClick={handleAdd}>Add</button>
-            <button onClick={handleReset}>Reset</button>
-            <button onClick={handleReorder}>Reorder</button>
-            <button>Save</button>
-            {submitActivity.length > 0  ? (
-                submitActivity.map((item,id)=>{
-                    const listedItem = <li key={item.id}>{item.activity}</li>
-                    return <ul>{listedItem}</ul>
+        ):(
+            <button disabled>Add</button>
+   
+        )
+        }
+        
+        {submitActivity.length > 0  ? (
+            submitActivity.map((item,id)=>{
+                const listedItem = <li key={item.id}>{item.activity}</li>
+                return <ul>{listedItem}</ul>
                 
                 })):(
                     <p className='lead'>
@@ -58,6 +74,10 @@ export default function App(){
                     </p>
                 )
             }
-        </>
+        <button className="btn btn-success" onClick={handleReset} style={{marginRight:10}}>Reset</button>
+        <button className= 'btn btn-secondary'onClick={handleReorder}>Reorder</button>
+        <button className="btn btn-primary" style={{marginLeft:10}}>Save</button>
+    </div>        
+</>
     )
 }
